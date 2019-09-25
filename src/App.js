65734b84f62f7d002c30ca4cd12b96566ce11c88
HanyMoh <hanymoh@msn.com>
2019-09-25 11:50:16 +0200

    Expected vs Unexpected Errors

--- src/App.js
+++ src/App.js
@@ -44,7 +44,20 @@ class App extends Component {
       throw new Error('')
     }
     catch (ex) {
-      alert('Something failed while deleting a post');
+      // Expected (404: not found, 400: bad request) - Client ERRORS
+      // - Display a specific error message
+      // 
+      // Unexpected (network down, server down, db down, bug)
+      // - Log them
+      // - Display a generic and friendly error message
+
+      if (ex.response && ex.response.status === 404)
+        alert('Something failed while deleting a post');
+      else {
+        console.log("Logging the error", ex);
+        alert("an unexpected error occurred");
+      }
+
       this.setState({ posts: orginalPosts });
     }
   };
